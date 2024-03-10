19355b279ca3e7a249d6eca7c645542dde3fbdb2
frazier0326 <frazier0326@gmail.com>
2024-03-09 20:12:43 -0600

    part 1 completed

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books of the Month";
+   let book1 = "https://m.media-amazon.com/images/I/61vBJ41AO5L._SL1200_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71tQ71QaNML._SL1500_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/81i7Yxg-X5L._SL1500_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="No Longer Human" />
+         <img src={book2} alt="If Cats Disappeared From The World" />
+         <img src={book3} alt="The Town With No Mirrors" />
       </div>      
    );
 }
