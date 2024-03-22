48d4bd7d1ac862ded195b8a8d88d855bf26d83dc
frazier0326 <frazier0326@gmail.com>
2024-03-21 20:42:07 -0500

    studio complete

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 
+const [boardName, setBoardName] = useState('no boards yet!');
+
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {
+         label: "Easy Meals",
+         value: "easyMeals"
+      },
+      {
+         label: "Recipes for Josh",
+         value: "recipesForJosh"
+      },
+      {
+         label: "Favorite Dishes",
+         value: "favoriteDishes"
+      }
+   ];
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
+
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+            <option value={board.value}>{board.label}</option>
+         ))}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://bellyfull.net/chicken-alfredo-recipe/";
+   let authorPhoto = "https://bellyfull.net/wp-content/uploads/2016/08/Author-Photo-blog.jpg";
+   let authorName = "Amy Flanigan";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Amy" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>Amy Flanigan's Website</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["chicken breasts", "italian seasoning", "fettuccine noodles", "alfredo sauce", "salt"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -36,8 +36,10 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Chicken Alfredo </h1>
+            <p>This easy Chicken Alfredo recipe includes golden pan-fried seasoned chicken 
+               breasts and tender noodles, coated in the most dreamy cream sauce ever. A showstopper 
+               meal and it’s all done in just over 30 minutes! </p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +51,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://bellyfull.net/wp-content/uploads/2021/02/Chicken-Alfredo-blog-4-768x1024.jpg" alt="" className="imageUpdates"/>
    );
 }
 
